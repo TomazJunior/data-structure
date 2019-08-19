2e4490c7dc4fdf2d90940bf935c3b9270efcc710
Tomaz Júnior <tomaz.jr@gmail.com>
2019-08-19 23:10:56 +0100

    Added the first problem

---
+++ 1/solution.js
@@ -0,0 +1,81 @@
+const assert = require('assert');
+
+/**
+ * Question:
+ * 
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ * 
+ */
+
+ /**
+  * Solution #1: O(n2)
+  * Interate through the array and test the sum with each subsequent value
+  */
+ function isAddUpTo(array, k) {
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      const value2 = array[j];
+      if (value + value2 === k) {
+        return true;
+      }
+    }
+  }
+  return false;
+ }
+
+ assert.equal(isAddUpTo([10, 15, 3, 7], 17), true);
+ assert.equal(isAddUpTo([], 10), false);
+ assert.equal(isAddUpTo([5, 3, 2, 15], 8), true);
+ assert.equal(isAddUpTo([1, 3, 2, 15], 1), false);
+
+ /**
+  * Solution #2: O(n)
+  * Sort the array in the ascending order and compare the sum of each pair
+  */
+ function isAddUpTo2(array, k) {
+  if (array.length < 2) { return false; }
+
+  // sort the array in ascending order
+  array.sort((a, b) => a-b);
+  let value = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] + value === k) {
+      return true;
+    }
+    value = array[i];
+  }
+  return false;
+ }
+
+ assert.equal(isAddUpTo2([10, 15, 3, 7], 17), true);
+ assert.equal(isAddUpTo2([], 10), false);
+ assert.equal(isAddUpTo2([5, 3, 2, 15], 8), true);
+ assert.equal(isAddUpTo2([1, 3, 2, 15], 1), false);
+ 
+
+  /**
+  * Solution #3: O(n)
+  * Store each visited value in a `Set` object and check if it contains the complement of the current value to obtain the target
+  */
+ function isAddUpTo3(array, k) {
+  if (array.length < 2) { return false; }
+
+  const values = new Set();
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    if (values.has(k - value)) {
+      return true;
+    }
+    values.add(value);
+  }
+  return false;
+ }
+
+ assert.equal(isAddUpTo3([10, 15, 3, 7], 10), true);
+ assert.equal(isAddUpTo3([], 10), false);
+ assert.equal(isAddUpTo3([5, 3, 2, 15], 8), true);
+ assert.equal(isAddUpTo3([5, 3, 2, 15], 1), false);
+
+ console.log('All tests passed!!');
